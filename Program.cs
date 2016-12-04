955d29ca9b50a63ae73ac7740ab5e152850d28b1
frigocoder <frigocoder@gmail.com>
2016-12-04 22:53:16 +0100

    - Added icon resource - Added systray icon

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace FastTab
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FastTabApplicationContext());
         }
     }
+
 }
