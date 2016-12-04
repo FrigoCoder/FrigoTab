0ac3b18df58004f8e2c92acac6da26a75ea3ab8d
Frigo <frigocoder@gmail.com>
2016-12-04 22:53:16 +0100

    - Added icon resource - Added systray icon

---
+++ FastTabApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using FastTab.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastTab
+{
+    class FastTabApplicationContext : ApplicationContext
+    {
+        private NotifyIcon notifyIcon;
+
+        public FastTabApplicationContext ()
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Resources.ocean_through_window_frame;
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[] { new MenuItem("Exit", Exit) });
+            notifyIcon.Visible = true;
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+
+}
