e48a571022676ad5b362159395e0a5e002691b84
Frigo <frigocoder@gmail.com>
2016-12-05 23:50:01 +0100

    - screwing around with keyboard hooks

--- Sources/Program.cs
+++ Sources/Program.cs
@@ -4,20 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FastTab
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+namespace FastTab {
+
+    static class Program {
+
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FastTabApplicationContext());
         }
+
     }
 
 }
