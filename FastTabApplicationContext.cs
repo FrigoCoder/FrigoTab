c1ad1268205991543fc244e3b5c8d538f8650da4
Frigo <frigocoder@gmail.com>
2016-12-05 14:11:28 +0100

    - Started adding alt-tab hook

--- FastTabApplicationContext.cs
+++ FastTabApplicationContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,12 +12,13 @@ namespace FastTab
 {
     class FastTabApplicationContext : ApplicationContext
     {
+        
         private NotifyIcon notifyIcon;
 
         public FastTabApplicationContext()
         {
             notifyIcon = new NotifyIcon
-            { 
+            {
                 Icon = Resources.ocean_through_window_frame,
                 ContextMenu = new ContextMenu(new MenuItem[] { new MenuItem("Exit", Exit) }),
                 Visible = true
