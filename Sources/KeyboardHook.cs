4a19e6628c76496fb05cd3b3ff99a34d8a53283d
frigocoder <frigocoder@gmail.com>
2016-12-16 00:27:20 +0100

    - reformatted

--- Sources/KeyboardHook.cs
+++ Sources/KeyboardHook.cs
@@ -1,86 +1,90 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Windows.Forms;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace FastTab {
 
     public class KeyboardHook : IDisposable {
 
-        public struct LPARAM {
-            public int vkCode;
-            public int scanCode;
-            public int flags;
-            public int time;
-            public int dwExtraInfo;
-        }
-
-        public delegate bool KeyCallback(IReadOnlyDictionary<Keys, bool> keys, int wParam, LPARAM lParam);
-        private delegate IntPtr KeyboardProc(int nCode, IntPtr wParam, ref LPARAM lParam);
+        public delegate bool KeyCallback (IReadOnlyDictionary<Keys, bool> keys, int wParam, LPARAM lParam);
 
         private const int WM_KEYDOWN = 0x0100;
         private const int WM_KEYUP = 0x0101;
         private const int WM_SYSKEYDOWN = 0x0104;
         private const int WM_SYSKEYUP = 0x0105;
 
-        private KeyCallback callback;
-        private KeyboardProc hookProc;
-        private IDictionary<Keys, bool> keys;
-        private IReadOnlyDictionary<Keys, bool> readOnlyKeys;
-        private IntPtr hookId;
+        private readonly KeyCallback callback;
+        private readonly IntPtr hookId;
+        private readonly KeyboardProc hookProc;
+        private readonly IDictionary<Keys, bool> keys;
+        private readonly IReadOnlyDictionary<Keys, bool> readOnlyKeys;
 
-        public KeyboardHook(KeyCallback callback) {
+        public KeyboardHook (KeyCallback callback) {
             this.callback = callback;
-            hookProc = new KeyboardProc(HookProc);
+            hookProc = HookProc;
 
             keys = new Dictionary<Keys, bool>();
-            foreach (Keys key in Enum.GetValues(typeof(Keys))) {
+            foreach( Keys key in Enum.GetValues(typeof(Keys)) ) {
                 keys[key] = false;
             }
+
             readOnlyKeys = new ReadOnlyDictionary<Keys, bool>(keys);
 
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule) {
-                hookId = SetWindowsHookEx(13, hookProc, GetModuleHandle(curModule.ModuleName), 0);
+            using( Process curProcess = Process.GetCurrentProcess() ) {
+                using( ProcessModule curModule = curProcess.MainModule ) {
+                    hookId = SetWindowsHookEx(13, hookProc, GetModuleHandle(curModule.ModuleName), 0);
+                }
             }
         }
 
-        private IntPtr HookProc(int nCode, IntPtr wParam, ref LPARAM lParam) {
-            if (nCode < 0) {
+        public void Dispose () {
+            UnhookWindowsHookEx(hookId);
+        }
+
+        private IntPtr HookProc (int nCode, IntPtr wParam, ref LPARAM lParam) {
+            if( nCode < 0 ) {
                 return CallNextHookEx(hookId, nCode, wParam, ref lParam);
             }
 
-            int w = (int)wParam;
-            Keys key = (Keys)lParam.vkCode;
-            if ((w == WM_KEYDOWN) || (w == WM_SYSKEYDOWN)) {
+            int w = (int) wParam;
+            Keys key = (Keys) lParam.vkCode;
+            if( (w == WM_KEYDOWN) || (w == WM_SYSKEYDOWN) ) {
                 keys[key] = true;
             }
-            if ((w == WM_KEYUP) || (w == WM_SYSKEYUP)) {
+            if( (w == WM_KEYUP) || (w == WM_SYSKEYUP) ) {
                 keys[key] = false;
             }
 
             bool callNext = callback(readOnlyKeys, w, lParam);
-            return callNext ? CallNextHookEx(hookId, nCode, wParam, ref lParam) : (IntPtr)1;
+            return callNext ? CallNextHookEx(hookId, nCode, wParam, ref lParam) : (IntPtr) 1;
         }
 
-        public void Dispose() {
-            UnhookWindowsHookEx(hookId);
+        [DllImport ("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle (string lpModuleName);
+
+        [DllImport ("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx (int idHook, KeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport ("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx (IntPtr hhk);
+
+        [DllImport ("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx (IntPtr hhk, int nCode, IntPtr wParam, ref LPARAM lParam);
+
+        public struct LPARAM {
+
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+
         }
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, ref LPARAM lParam);
+        private delegate IntPtr KeyboardProc (int nCode, IntPtr wParam, ref LPARAM lParam);
 
     }
 
--- Sources/Program.cs
+++ Sources/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿#region
+
+using System;
 using System.Windows.Forms;
+
+#endregion
 
 namespace FastTab {
 
-    static class Program {
+    internal static class Program {
 
         [STAThread]
-        static void Main() {
+        private static void Main () {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FastTabApplicationContext());
