091829a1f87db1c1095456513c38cd85ea6e5b0b
Frigo <frigocoder@gmail.com>
2017-02-04 16:59:15 +0100

    - extracted SysTrayIcon from FastTabApplicationContext

---
+++ SysTrayIcon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FrigoTab {
+
+    public class SysTrayIcon : IDisposable {
+
+        private readonly NotifyIcon _notifyIcon;
+
+        public event Action Exit;
+
+        public SysTrayIcon () {
+            _notifyIcon = new NotifyIcon {
+                Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath),
+                ContextMenu = new ContextMenu(new[] {
+                    new MenuItem("Exit", ExitHandler)
+                }),
+                Visible = true
+            };
+        }
+
+        public void ExitHandler (object sender, EventArgs args) {
+            Dispose();
+            Exit?.Invoke();
+        }
+
+        public void Dispose () {
+            _notifyIcon.Visible = false;
+            _notifyIcon.Dispose();
+        }
+
+    }
+
+}
