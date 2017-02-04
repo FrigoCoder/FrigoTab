4524d9a5b42f372f0fc4a06ef9de79f779104690
frigocoder <frigocoder@gmail.com>
2017-02-04 14:25:33 +0100

    - KeyboardHook now has an event instead of a single callback - we pass a Key and the state of the alt

--- KeyboardHookEventArgs.cs
+++ KeyboardHookEventArgs.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace FrigoTab {
 
     public class KeyboardHookEventArgs {
 
+        public readonly Keys Key;
+        public readonly bool Alt;
         public bool Handled;
 
-        public Dictionary<Keys, bool> Keys;
+        public KeyboardHookEventArgs (Keys key, bool alt) {
+            Key = key;
+            Alt = alt;
+        }
 
     }
 
