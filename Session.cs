19e5ca68c10fabe8421db183fc6d6a825934c6ee
frigocoder <frigocoder@gmail.com>
2017-02-11 13:35:14 +0100

    - displaying tool windows as well

--- Session.cs
+++ Session.cs
@@ -8,21 +8,29 @@ namespace FrigoTab {
 
     public class Session : Form, IDisposable {
 
+        private readonly IList<Thumbnail> _toolwindows = new List<Thumbnail>();
         private readonly IList<Thumbnail> _thumbnails = new List<Thumbnail>();
 
         public Session () {
             Bounds = Screen.AllScreens.Select(screen => screen.Bounds).Aggregate(Rectangle.Union);
 
-
             WindowFinder finder = new WindowFinder();
+
+            foreach( WindowHandle window in finder.ToolWindows ) {
+                _toolwindows.Add(new Thumbnail(window, Handle, window.GetWindowRect()));
+            }
             foreach( WindowHandle window in finder.Windows ) {
                 _thumbnails.Add(new Thumbnail(window, Handle, window.GetWindowRect()));
             }
+
             Visible = true;
         }
 
         public new void Dispose () {
             Visible = false;
+            foreach( Thumbnail thumbnail in _toolwindows ) {
+                thumbnail.Dispose();
+            }
             foreach( Thumbnail thumbnail in _thumbnails ) {
                 thumbnail.Dispose();
             }
