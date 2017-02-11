38f5450bddec26115b53b537adee4c91558d214a
Frigo <frigocoder@gmail.com>
2017-02-11 13:35:14 +0100

    - displaying tool windows as well

--- WindowFinder.cs
+++ WindowFinder.cs
@@ -7,6 +7,7 @@ namespace FrigoTab {
     public class WindowFinder {
 
         public readonly IList<WindowHandle> Windows = new List<WindowHandle>();
+        public readonly IList<WindowHandle> ToolWindows = new List<WindowHandle>();
 
         public WindowFinder () {
             EnumWindows(EnumWindowCallback, IntPtr.Zero);
@@ -15,6 +16,9 @@ namespace FrigoTab {
         private bool EnumWindowCallback (WindowHandle handle, IntPtr lParam) {
             if( IsWindow(handle) ) {
                 Windows.Add(handle);
+            }
+            if( IsToolWindow(handle) ) {
+                ToolWindows.Add(handle);
             }
             return true;
         }
@@ -41,6 +45,28 @@ namespace FrigoTab {
                 return false;
             }
             return true;
+        }
+
+        private static bool IsToolWindow (WindowHandle handle) {
+            WindowStyles style = handle.GetWindowStyles();
+            if( style.HasFlag(WindowStyles.Disabled) ) {
+                return false;
+            }
+            if( !style.HasFlag(WindowStyles.Visible) ) {
+                return false;
+            }
+
+            WindowExStyles ex = handle.GetWindowExStyles();
+            if( ex.HasFlag(WindowExStyles.NoActivate) ) {
+                return false;
+            }
+            if( ex.HasFlag(WindowExStyles.AppWindow) ) {
+                return false;
+            }
+            if( ex.HasFlag(WindowExStyles.ToolWindow) ) {
+                return true;
+            }
+            return false;
         }
 
         [DllImport ("user32.dll")]
