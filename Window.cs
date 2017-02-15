017f728413f6822f4365a71c44d22ebc5d4a5e58
Frigo <frigocoder@gmail.com>
2017-02-15 15:09:10 +0100

    - extracted Window that contains thumbnail and bounds

---
+++ Window.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace FrigoTab {
+
+    public class Window : IDisposable {
+
+        public readonly WindowHandle Handle;
+        public readonly Rectangle Bounds;
+        private readonly Thumbnail _thumbnail;
+
+        public Window (WindowHandle owner, WindowHandle handle, Rectangle bounds) {
+            Handle = handle;
+            Bounds = bounds;
+            _thumbnail = new Thumbnail(handle, owner, new Rect(bounds));
+        }
+
+        public void Dispose () {
+            _thumbnail.Dispose();
+        }
+
+    }
+
+}
