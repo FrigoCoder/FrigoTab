ca6e7f226b7d127cb8a5c750c7817da9a308b8c2
Frigo <frigocoder@gmail.com>
2017-02-23 11:55:19 +0100

    - fixed bug where the application does not correctly set the client area, and our call to ScreenToClient gives wrong coordinates

--- Thumbnail.cs
+++ Thumbnail.cs
@@ -16,7 +16,7 @@ namespace FrigoTab {
             DwmRegisterThumbnail(destination, source, out _thumbnail);
             DwmThumbnailProperties properties = new DwmThumbnailProperties {
                 Flags = DwmThumbnailFlags.RectSource | DwmThumbnailFlags.RectDestination,
-                Source = ScreenToClient(source, source.GetRect()),
+                Source = new ClientRect(Point.Empty, source.GetRect().Size),
                 Destination = ScreenToClient(destination, bounds)
             };
             DwmUpdateThumbnailProperties(_thumbnail, ref properties);
