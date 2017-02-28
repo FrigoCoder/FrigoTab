5aa14d479e99d62ffe56888d240f530d1dca478a
Frigo <frigocoder@gmail.com>
2017-02-28 17:02:38 +0100

    - moved KeyHookEventArgs into KeyHook file - moved MouseHookEventArgs into MouseHook file

--- KeyHook.cs
+++ KeyHook.cs
@@ -6,6 +6,19 @@ using System.Windows.Forms;
 
 namespace FrigoTab {
 
+    public class KeyHookEventArgs {
+
+        public readonly Keys Key;
+        public readonly bool Alt;
+        public bool Handled;
+
+        public KeyHookEventArgs (Keys key, bool alt) {
+            Key = key;
+            Alt = alt;
+        }
+
+    }
+
     public class KeyHook : IDisposable {
 
         [SuppressMessage ("ReSharper", "PrivateFieldCanBeConvertedToLocalVariable")]
--- MouseHook.cs
+++ MouseHook.cs
@@ -6,6 +6,19 @@ using System.Runtime.InteropServices;
 
 namespace FrigoTab {
 
+    public class MouseHookEventArgs {
+
+        public readonly Point Point;
+        public readonly bool Click;
+        public bool Handled;
+
+        public MouseHookEventArgs (Point point, bool click) {
+            Point = point;
+            Click = click;
+        }
+
+    }
+
     public class MouseHook : IDisposable {
 
         [SuppressMessage ("ReSharper", "PrivateFieldCanBeConvertedToLocalVariable")]
