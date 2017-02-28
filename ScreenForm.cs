96f925fe4c02d70876a919a482645f878d3ab9ee
Frigo <frigocoder@gmail.com>
2017-02-28 22:33:31 +0100

    - introduced ScreenForms

---
+++ ScreenForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FrigoTab {
+
+    public class ScreenForm : FrigoForm, IDisposable {
+
+        public ScreenForm (Form owner, Screen screen) {
+            Owner = owner;
+            Bounds = screen.WorkingArea;
+        }
+
+        public new void Dispose () {
+            Close();
+        }
+
+    }
+
+}
