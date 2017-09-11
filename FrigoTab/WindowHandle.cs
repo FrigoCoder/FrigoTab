dabbaa1228e5ded2d2a5938e86f39981b355e69e
Frigo <frigocoder@gmail.com>
2017-09-11 22:05:49 +0200

    Backed out changeset: 1fd63b5b070d

--- FrigoTab/WindowHandle.cs
+++ FrigoTab/WindowHandle.cs
@@ -51,9 +51,9 @@ namespace FrigoTab {
         }
 
         public ScreenRect GetWindowRect () {
-            ScreenRect rect;
-            GetWindowRect(_handle, out rect);
-            return rect;
+            ScreenRect lpRect;
+            GetWindowRect(_handle, out lpRect);
+            return lpRect;
         }
 
         public void SetForeground () {
@@ -168,6 +168,9 @@ namespace FrigoTab {
         }
 
         [DllImport("user32.dll")]
+        private static extern bool GetWindowRect (IntPtr hWnd, out ScreenRect lpRect);
+
+        [DllImport("user32.dll")]
         private static extern int GetWindowTextLength (IntPtr hWnd);
 
         [DllImport("user32.dll")]
@@ -213,9 +216,6 @@ namespace FrigoTab {
             DwmWindowAttribute dwAttribute,
             out bool pvAttribute,
             int cbAttribute);
-
-        [DllImport("user32.dll")]
-        private static extern bool GetWindowRect (IntPtr hWnd, out ScreenRect lpRect);
 
     }
 
